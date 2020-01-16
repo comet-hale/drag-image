cf619fff3f00600bf31a17be18f3dff18ff842e3
muhammed-g <johnsmith93030@gmail.com>
2020-01-16 17:39:47 +0800

    Updates

---
+++ precache-manifest.b650f127400ee6ef26b92f43a45d7d2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007ebd154e7c3c722833accc864e460b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a2ea7b0d2467b6cfe69",
+    "url": "/static/css/main.83d6086c.chunk.css"
+  },
+  {
+    "revision": "9a9c1c6974f66b06b81c",
+    "url": "/static/js/2.eb2c62a6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.eb2c62a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a2ea7b0d2467b6cfe69",
+    "url": "/static/js/main.37f00541.chunk.js"
+  },
+  {
+    "revision": "b02aa6474ebb62fffa37",
+    "url": "/static/js/runtime-main.5c1c31e6.js"
+  }
+]);
